7b3c2153af02622f5981e0f2615ef85f6b74f280
SheikhAlUsama <82088182+SheikhAlUsama@users.noreply.github.com>
2021-04-22 04:25:03 +0500

    Add files via upload
    ramazan calendar 2021

---
+++ script.js
@@ -0,0 +1,100 @@
+//                                FASTING TIME CALENDAR
+
+var time = +prompt("TIME OF IFTAR AND SEHR (Enter Number for date)")
+var num = ["1st Roza", "2nd Roza", "3rd Roza", "4th Roza", "5th Roza", "6th Roza", "7th Roza", "8th Roza", "9th Roza", "10th Roza", "11th Roza", "12th Roza", "13th Roza", "14th Roza", "15th Roza", "16th Roza", "17th Roza", "18th Roza", "19th Roza", "20th Roza", "21th Roza", "22th Roza", "23th Roza", "24th Roza", "25th Roza", "26th Roza", "27th Roza", "28th Roza", "29th Roza", "30th Roza",]
+var sehr = ["4:51am", "4:50am", "4:49am", "4:48am", "4:47am", "4:46am", "4:45am", "4:44am", "4:43am", "4:42am", "4:41am", "4:40am", "4:39am", "4:38am", "4:37am", "4:36am", "4:35am", "4:34am", "4:33am", "4:32am", "4:31am", "4:30am", "4:29am", "4:28am", "4:28am", "4:27am", "4:26am", "4:25am", "4:24am", "4:24am"]
+var iftar = ["6:55pm", "6:55pm", "6:56pm", "6:56pm", "6:57pm", "6:57pm", "6:57pm", "6:58pm", "6:58pm", "6:59pm", "6:59pm", "7:00pm", "7:00pm", "7:01pm", "7:01pm", "7:02pm", "7:02pm", "7:03pm", "7:03pm", "7:04pm", "7:04pm", "7:05pm", "7:05pm", "7:06pm", "7:06pm", "7:07pm", "7:07pm", "7:08pm", "7:08pm", "7:09pm"]
+
+if (time === 1) {
+    document.write("<h1>In " + num[0] + "<br/></h1>" + " <h3>Sehr time is " + sehr[0] + "" + "<br/>" + " Iftar time is " + iftar[0] + "</h3>")
+}
+if (time === 2) {
+    document.write("<h1>In " + num[1] + "<br/></h1>" + " <h3>Sehr time is " + sehr[1] + "" + "<br/>" + " Iftar time is " + iftar[1] + "</h3>")
+}
+if (time === 3) {
+    document.write("<h1>In " + num[2] + "<br/></h1>" + " <h3>Sehr time is " + sehr[2] + "" + "<br/>" + " Iftar time is " + iftar[2] + "</h3>")
+}
+if (time === 4) {
+    document.write("<h1>In " + num[3] + "<br/></h1>" + " <h3>Sehr time is " + sehr[3] + "" + "<br/>" + " Iftar time is " + iftar[3] + "</h3>")
+}
+if (time === 5) {
+    document.write("<h1>In " + num[4] + "<br/></h1>" + " <h3>Sehr time is " + sehr[4] + "" + "<br/>" + " Iftar time is " + iftar[4] + "</h3>")
+}
+if (time === 6) {
+    document.write("<h1>In " + num[5] + "<br/></h1>" + " <h3>Sehr time is " + sehr[5] + "" + "<br/>" + " Iftar time is " + iftar[5] + "</h3>")
+}
+if (time === 7) {
+    document.write("<h1>In " + num[6] + "<br/></h1>" + " <h3>Sehr time is " + sehr[6] + "" + "<br/>" + " Iftar time is " + iftar[6] + "</h3>")
+}
+if (time === 8) {
+    document.write("<h1>In " + num[7] + "<br/></h1>" + " <h3>Sehr time is " + sehr[7] + "" + "<br/>" + " Iftar time is " + iftar[7] + "</h3>")
+}
+if (time === 9) {
+    document.write("<h1>In " + num[8] + "<br/></h1>" + " <h3>Sehr time is " + sehr[8] + "" + "<br/>" + " Iftar time is " + iftar[8] + "</h3>")
+}
+if (time === 10) {
+    document.write("<h1>In " + num[9] + "<br/></h1>" + " <h3>Sehr time is " + sehr[9] + "" + "<br/>" + " Iftar time is " + iftar[9] + "</h3>")
+}
+if (time === 11) {
+    document.write("<h1>In " + num[10] + "<br/></h1>" + " <h3>Sehr time is " + sehr[10] + "" + "<br/>" + " Iftar time is " + iftar[10] + "</h3>")
+}
+if (time === 12) {
+    document.write("<h1>In " + num[11] + "<br/></h1>" + " <h3>Sehr time is " + sehr[11] + "" + "<br/>" + " Iftar time is " + iftar[11] + "</h3>")
+}
+if (time === 13) {
+    document.write("<h1>In " + num[12] + "<br/></h1>" + " <h3>Sehr time is " + sehr[12] + "" + "<br/>" + " Iftar time is " + iftar[12] + "</h3>")
+}
+if (time === 14) {
+    document.write("<h1>In " + num[13] + "<br/></h1>" + " <h3>Sehr time is " + sehr[13] + "" + "<br/>" + " Iftar time is " + iftar[13] + "</h3>")
+}
+if (time === 15) {
+    document.write("<h1>In " + num[14] + "<br/></h1>" + " <h3>Sehr time is " + sehr[14] + "" + "<br/>" + " Iftar time is " + iftar[14] + "</h3>")
+}
+if (time === 16) {
+    document.write("<h1>In " + num[15] + "<br/></h1>" + " <h3>Sehr time is " + sehr[15] + "" + "<br/>" + " Iftar time is " + iftar[15] + "</h3>")
+}
+if (time === 17) {
+    document.write("<h1>In " + num[16] + "<br/></h1>" + " <h3>Sehr time is " + sehr[16] + "" + "<br/>" + " Iftar time is " + iftar[16] + "</h3>")
+}
+if (time === 18) {
+    document.write("<h1>In " + num[17] + "<br/></h1>" + " <h3>Sehr time is " + sehr[17] + "" + "<br/>" + " Iftar time is " + iftar[17] + "</h3>")
+}
+if (time === 19) {
+    document.write("<h1>In " + num[18] + "<br/></h1>" + " <h3>Sehr time is " + sehr[18] + "" + "<br/>" + " Iftar time is " + iftar[18] + "</h3>")
+}
+if (time === 20) {
+    document.write("<h1>In " + num[19] + "<br/></h1>" + " <h3>Sehr time is " + sehr[19] + "" + "<br/>" + " Iftar time is " + iftar[19] + "</h3>")
+}
+if (time === 21) {
+    document.write("<h1>In " + num[20] + "<br/></h1>" + " <h3>Sehr time is " + sehr[20] + "" + "<br/>" + " Iftar time is " + iftar[20] + "</h3>")
+}
+if (time === 22) {
+    document.write("<h1>In " + num[21] + "<br/></h1>" + " <h3>Sehr time is " + sehr[21] + "" + "<br/>" + " Iftar time is " + iftar[21] + "</h3>")
+}
+if (time === 23) {
+    document.write("<h1>In " + num[22] + "<br/></h1>" + " <h3>Sehr time is " + sehr[22] + "" + "<br/>" + " Iftar time is " + iftar[22] + "</h3>")
+}
+if (time === 24) {
+    document.write("<h1>In " + num[23] + "<br/></h1>" + " <h3>Sehr time is " + sehr[23] + "" + "<br/>" + " Iftar time is " + iftar[23] + "</h3>")
+}
+if (time === 25) {
+    document.write("<h1>In " + num[24] + "<br/></h1>" + " <h3>Sehr time is " + sehr[24] + "" + "<br/>" + " Iftar time is " + iftar[24] + "</h3>")
+}
+if (time === 26) {
+    document.write("<h1>In " + num[25] + "<br/></h1>" + " <h3>Sehr time is " + sehr[25] + "" + "<br/>" + " Iftar time is " + iftar[25] + "</h3>")
+}
+if (time === 27) {
+    document.write("<h1>In " + num[26] + "<br/></h1>" + " <h3>Sehr time is " + sehr[26] + "" + "<br/>" + " Iftar time is " + iftar[26] + "</h3>")
+}
+if (time === 28) {
+    document.write("<h1>In " + num[27] + "<br/></h1>" + " <h3>Sehr time is " + sehr[27] + "" + "<br/>" + " Iftar time is " + iftar[27] + "</h3>")
+}
+if (time === 29) {
+    document.write("<h1>In " + num[28] + "<br/></h1>" + " <h3>Sehr time is " + sehr[28] + "" + "<br/>" + " Iftar time is " + iftar[28] + "</h3>")
+}
+if (time === 30) {
+    document.write("<h1>In " + num[29] + "<br/></h1>" + " <h3>Sehr time is " + sehr[29] + "" + "<br/>" + " Iftar time is " + iftar[29] + "</h3>")
+}
+ else if (time !== time){
+    alert("Please Enter correct date")
+}
